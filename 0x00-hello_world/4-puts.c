7afdac1e1a49b16fb0841a146e586222c869a774
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-15 03:15:31 -0700

    print task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ *main-Main is the first function of the program
+ *Return: this returns zero by default unless stated otherwise
+ */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return(0);
+}
