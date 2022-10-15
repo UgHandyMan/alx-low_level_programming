e87222d894d910ac157a6082a28712d87013b243
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-15 14:17:53 -0700

    final task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - main serves as the starting point for program execution
+ *Return: returns zero by default
+ */
+int main(void)
+{
+int i;
+for (i = '0'; 1 <= '9'; i++)
+{
+putchar(i);
+if (1 != '9')
+{
+putchar(',');
+putchar('');
+}
+}
+putchar('\n');
+return (0);
+}
