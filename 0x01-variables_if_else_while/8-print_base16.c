53731980f09caad896f4dfd7ecad50a173c7de31
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-15 14:01:35 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unisd.h>
+/**
+* main - main serves as the starting point for program execution
+*Return: returns zero by default
+*/
+int main(void)
+{
+char c;
+char d = '0'
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
