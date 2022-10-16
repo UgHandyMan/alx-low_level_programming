66c97073b30bfe8b69959fd342c0204928006a27
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-16 21:14:34 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 /**
  * main - main serves as the starting point for program execution
  *Return: returns zero by default
  */
 int main(void)
 {
-  int c, i;
-  for (c = '0'; c <= '98'; c++)
-    {
-      for (i = '0'; i <= '99'; i++)
-	{
-	  if (c < i)
-	    {
-	      putchar(c);
-	      putchar(i);
-	      if (c == '98' && i == '99')
-		{
-		  putchar(',');
-		    putchar(' ');
-
-		}
-	    }
-	}
-    }
-  putchar('\n');
-  return(0);
+int c, i;
+for (c = '0'; c <= '98'; c++)
+{
+for (i = '0'; i <= '99'; i++)
+{
+if (c < i)
+{
+putchar(c);
+putchar(i);
+if (c == '98' && i == '99')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return(0);
 }
