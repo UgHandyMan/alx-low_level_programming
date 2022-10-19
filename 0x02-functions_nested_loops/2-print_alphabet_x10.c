c358d052b3ebf79c5b9e93d31c8f9d58c91c31d2
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-19 09:46:44 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - main serves as the starting point for program execution
+*Return: returns zero by default
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar (ch)
+putchar ('\n');
+return (0);
+}
