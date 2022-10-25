6630881f378275ccf74ea2ef004416be49c574a4
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-25 10:38:53 -0700

    correction

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - updates the value as main function
+ *Return: returns zero by default unless defined otherwise
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98
+}
