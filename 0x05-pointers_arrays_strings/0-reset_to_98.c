e3e8f50afa2f545db5bcf1313267bc7b4d173f4b
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-25 10:42:23 -0700

    corrections

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,11 @@
 
 /**
  *reset_to_98 - updates the value as main function
+n - variable to be printed
  *Return: returns zero by default unless defined otherwise
  */
 
 void reset_to_98(int *n)
 {
-*n = 98
+*n = 98;
 }
