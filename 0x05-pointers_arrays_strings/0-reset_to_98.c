c34b39b93d1bce0a0a167e186f010856c6854612
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-25 21:05:42 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- *reset_to_98 - updates the value as main function
-'n': input  variable to be printed
- *Return: returns zero by default unless defined otherwise
- */
+*reset_to_98 - updates the value as main function
+*Description: a function that takes a pointer to an int
+*@n: input  variable to be printed
+*Return: returns zero by default unless defined otherwise
+*/
 
 void reset_to_98(int *n)
 {
