67e4308be91aa31ca27e4849178d9750ef726c36
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-25 13:41:16 -0700

    fifth task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *rev_string - reverses a string variable
+ *@s: input variable
+ *Returns string in reverse
+ */
+void rev_string(char *s)
+{
+  char r = s[0];
+  int x = 0;
+  int i;
+
+  while s[x] != '\0')
+  x++;
+for (i = 0; i < x; i++)
+  {
+    c--;
+    r = s[i];
+    s[i] = s[x];
+    s[x] = r;
+  }
+}
