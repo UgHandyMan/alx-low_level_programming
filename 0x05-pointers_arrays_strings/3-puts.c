cb46b1ba1d93db84a6704adf219aeee68d101011
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-25 22:44:10 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
-#include "main.h"
-
-/**
-*_puts - prints a string then new line
-*@str: string variable to be printed
-*Return: returns zero by default
-*/
-void _puts(char *str)
-{
-str = "I do not fear computers. I fear the lack of them - Isaac Asimov"
-_putchar(*str++);
-
-_putchar('\n');
-}
+#include "main.h"                                                                                                                                                    
+#include <stdio.h>                                                                                                                                                   
+                                                                                                                                                                     
+/**                                                                                                                                                                  
+*_puts - prints a string then new line                                                                                                                               
+*@str: string variable to be printed                                                                                                                                 
+*Return: returns zero by default                                                                                                                                     
+*/                                                                                                                                                                   
+void _puts(char *str)                                                                                                                                                
+{                                                                                                                                                                    
+while (str != 0)                                                                                                                                                  
+{                                                                                                                                                                    
+_putchar(*str++);                                                                                                                                                    
+}                                                                                                                                                                    
+_putchar('\n')                                                                                                                                                       
+}                                                                                                                                                                    
+                    
