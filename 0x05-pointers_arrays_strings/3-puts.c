b8879ddfb681c5a149c2529480752ad85d7633b4
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-25 12:29:18 -0700

    fourth task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts - prints a string then new line
+*@str: string variable to be printed
+*Return: returns zero by default
+*/
+void _puts(char *str)
+{
+while (str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n')
+}
