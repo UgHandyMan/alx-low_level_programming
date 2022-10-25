703e4395f0546a640449bfade643f69883d56962
UgHandyMan <fredkawesi@gmail.com.com>
2022-10-25 11:41:37 -0700

    task two

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*swap_int - swaps the values of two intergers
+*@a: iterger a
+*@b: interger b
+*Return: returns zero by default
+*/
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = q;
+}
