4ad2254117cdb90e250f6ef0f3f9600cd5fcb423
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-25 22:49:13 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
-#include "main.h"                                                                                                                                                    
-#include <stdio.h>                                                                                                                                                   
-                                                                                                                                                                     
-/**                                                                                                                                                                  
-*_puts - prints a string then new line                                                                                                                               
-*@str: string variable to be printed                                                                                                                                 
-*Return: returns zero by default                                                                                                                                     
-*/                                                                                                                                                                   
-void _puts(char *str)                                                                                                                                                
-{                                                                                                                                                                    
-while (str != 0)                                                                                                                                                  
-{                                                                                                                                                                    
-_putchar(*str++);                                                                                                                                                    
-}                                                                                                                                                                    
-_putchar('\n');                                                                                                                                                       
-}                                                                                                                                                                    
-                    
+#include "main.h"
+/**
+*_puts - prints a string then new line
+*@str: string variable to be printed
+*Return: returns zero by default
+*/
+void _puts(char *str)
+{
+while (str != 0)
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
+
