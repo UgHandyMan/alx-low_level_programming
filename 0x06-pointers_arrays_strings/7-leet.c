59fec9c1fc75abe0e5c533e5ab8f45cef41bb9e1
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-27 23:06:48 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
 /**
 *leet - encodes a string into a numerical value
 *@n: input
 *Return: returns an encoded value
 */
+
 char *leet(char *)
 {
 int i, j;
-char s1[] = "aAeEoOtTlL"
-char s2[] = "4433007711"
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711";
 for (i = 0; n[i] != '\0'; i++)
 {
 if (n[i] == s1[j]
