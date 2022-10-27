2c726279c42e39f42a4ab4be81122e65f949cc5e
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-27 23:13:27 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,33 +3,33 @@
 #include<unistd.h>
 
 /**
- *cap_string - capitalizes all words of a string.
- *@str: string variable
- *Return: returns a string
- */
+*cap_string - capitalizes all words of a string.
+*@str: string variable
+*Return: returns a string
+*/
 char *cap_string(char *)
 {
-  int i = 0;
-  while (str[i])
-    {
-      while (!(str[i] >= 'a' && str[i] <= 'z'))
-	i++;
-      if (str[i-1] == ' ' ||
-	  str[i-1] == '\t' ||
-	   str[i-1] == '\n' ||
-	  str[i-1] == ',' ||
-	  str[i-1] == ';' ||
-	  str[i-1] == '.' ||
-	  str[i-1] == '!' ||
-	  str[i-1] == '?' ||
-	  str[i-1] == '"' ||
-	  str[i-1] == '(' ||
-	  str[i-1] == ')' ||
-	  str[i-1] == '{' ||
-	  str[i-1] == '}' ||
-	  i = 0;
-	  str[i] -= 32;
-	  i++;
-	  }
-      return (str);
-    }
+int i = 0;
+while (str[i])
+{
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+i++;
+if (str[i-1] == ' ' ||
+str[i-1] == '\t' ||
+str[i-1] == '\n' ||
+str[i-1] == ',' ||
+tr[i-1] == ';' ||
+str[i-1] == '.' ||
+str[i-1] == '!' ||
+str[i-1] == '?' ||
+str[i-1] == '"' ||
+str[i-1] == '(' ||
+str[i-1] == ')' ||
+str[i-1] == '{' ||
+str[i-1] == '}' ||
+i = 0;
+str[i] -= 32;
+i++;
+}
+return (str);
+}
