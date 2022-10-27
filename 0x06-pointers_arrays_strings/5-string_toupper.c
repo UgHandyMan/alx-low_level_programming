0e8cf03dbc94fa04d5343c8f783c49998b82b521
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-27 23:15:23 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,20 +3,19 @@
 #include<unistd.h>
 
 /**
- *string_toupper - changes all lowercase letters of a string to uppercase.
- *@n: pointer
- *Return: returns upper case characters
- */
+*string_toupper - changes all lowercase letters of a string to uppercase.
+*@n: pointer
+*Return: returns upper case characters
+*/
 char *string_toupper(char *)
 {
-  int i;
-
-  i = 0;
-  while (n[i] != '\0')
-    {
-      if (n[i] >= 'a' && n[i] <= 'z')
-	n[i] = n[i] - 32;
-      i++
-	}
-  return (n);
+int i;
+i = 0;
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+n[i] = n[i] - 32;
+i++
+}
+return (n);
 }
