69d6e428a9ae6fd48f267722e408dce6e751deeb
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-10-27 23:33:21 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,24 @@
 #include "main.h"
-#include<stdio.h>
-#include<unistd.h>
 
 /**
- *_strncat -  concatenates two strings.
- *@dest: destination file
- *@src: source file
- *@n: number of characters to be copied
- *Return: returns zero by default
- */
-
+*_strncat -  concatenates two strings.
+*@dest: destination file
+*@src: source file
+*@n: number of characters to be copied
+*Return: returns zero by default
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i;
-
-  i = 0;
-  while (i < n && src[i] != '\0')
-    dest[i] = src[i];
-  i++;
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+dest[i] = src[i];
+i++;
 }
 while (i < n)
-  {
-    dest[i] = '\0';
-    i++;
-  }
+{
+dest[i] = '\0';
+i++;
+}
 return (dest);
 }
