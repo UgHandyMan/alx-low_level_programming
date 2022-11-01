66963c753a889ed77badde40d4c58b2396e90a8d
UgHandyMan <fredkawesi@gmail.com.com>
2022-11-01 13:05:42 -0700

    first task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_memset - function fills the bytes of the memoryarea pointed
+*@s: starting address of the memory to print
+*@n: size of the memory to print
+*@b: constant
+*Return: returns zero by default
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+for (; n > 0; i++)   
+{
+s[i] = b;  
+n--;
+}
+return (s);  
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is the read me file to the third pointers and arrays task
