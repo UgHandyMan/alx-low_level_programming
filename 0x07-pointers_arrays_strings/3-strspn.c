324f333aa71465881fa45daf4d51971f482a3493
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-01 23:49:59 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *_strspn - Entry point
 *@s: input
 *@accept: input
 *Return: Always 0 (Success)
 */
-unsigned int _strspn(char *s, char *accept) 
+unsigned int _strspn(char *s, char *accept)
 {
 unsigned int n = 0;
 int r;
@@ -23,5 +24,5 @@ return (n);
 }
 s++;
 }
-return (n); 
+return (n);
 }
