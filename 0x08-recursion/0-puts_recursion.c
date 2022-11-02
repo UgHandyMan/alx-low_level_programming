91437a0842ef846d285c470cf43d0e368e41bef4
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-02 22:03:04 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,21 @@
 #include <unistd.h>
 
 /**
- *_puts_recursion - prints a string
- *@s: String to be printed
- *Return: returns zero by default
- */
+*_puts_recursion - prints a string
+*@s: string to be printed
+*Return: returns zero by default
+*/
 void _puts_recursion(char *s)
 {
-  if (char *s == 0)
-    {
-      _putchar('\n');
-      return 0;
-    }
-  _putchar(*s);
-  _puts_recursion(s+1);
+if (*s == 0)
+{
+_putchar('\n');
+return 0;
+}
+while (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s+1);
+s++;
+}
+}
