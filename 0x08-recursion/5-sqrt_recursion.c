f975951e63e132bcb235290a94d5aca47475b03c
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-03 23:06:46 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,12 @@
  *
  *Return: the resulting square root
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt_recursion(int n)
 {
+int i;
 if (i * i < n)
 return (-1);
 if (i * i == n)
 return (i);
 return (_sqrt_recursion(n, i + 1));
 }
-
