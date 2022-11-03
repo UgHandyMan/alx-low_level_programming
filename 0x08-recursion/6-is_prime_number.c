c8a5c49580cd0b767edab480b48a1a060925f668
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-03 23:05:04 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,10 +11,8 @@ int is_prime_number(int n)
 {
 int i;
 if (i == 1)
-{
 return (1);
 if (n % i == 0 && i > 0)
 return (0);
-return (is_prime_number(n, i - 1));
+return (prime_number(n, i - 1));
 } 
-}
