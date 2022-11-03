9164be17ea989bc2285e2b16621c32d1f9387084
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-03 22:57:15 +0300

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,11 +3,11 @@
 /**
 * is_prime_number - says if an integer is a prime number or not
 * @n: number to evaluate
-*
+* @i: iterator
 * Return: 1 if n is a prime number, 0 if not
 */
 
-int is_prime_number(int n)
+int is_prime_number(int n, int i)
 {
 if (i == 1)
 return (1);
