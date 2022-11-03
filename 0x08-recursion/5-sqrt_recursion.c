0c899d9437bfc6928e1eb3a115815e90e9c37946
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-03 22:37:45 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,16 @@
 /**
- * _sqrt_recursion - recurses to find the natural
- * square root of a number
- * @n: number to calculate the sqaure root of
- * @i: iterator
- *
- * Return: the resulting square root
- */
+* _sqrt_recursion - recurses to find the natural
+* square root of a number
+* @n: number to calculate the sqaure root of
+* @i: iterator
+*
+* Return: the resulting square root
+*/
 int _sqrt_recursion(int n, int i)
 {
-	if (i * i > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (_sqrt_recursion(n, i + 1));
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt_recursion(n, i + 1));
 }
