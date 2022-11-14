23896ed11d6591f8ab69b5f9da9d43143d0ac0fc
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-14 15:07:47 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  *main -  that prints the number of arguments passed into it.
  *@argc: number of arguments
  *@argv: array of arguments
  *Returns: zero by default
  */
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int count;
-printf ("This program was called with \"%s\".\n", argv[0]);
+printf("This program was called with \"%s\".\n", argv[0]);
 if (argc > 1)
 {
-  for (count = 1; count < argc; count++)
+for (count = 1; count < argc; count++)
 {
-printf("argv[%d] = %s\n", count, argv[count]); 
+printf("argv[%d] = %s\n", count, argv[count]);
 }
 }
 else
 {
-printf("The command had no other arguments.\n"); 
-}  
+printf("The command had no other arguments.\n");
+}
 return 0;
 }
