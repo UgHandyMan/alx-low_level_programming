bf2f4b3c4250cbc5cca80aa1113b9d3d498d0e3b
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-19 16:02:33 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,8 +5,6 @@
 /**
 *sum_them_all -  returns the sum of all its parameters.
 *@n: parameter
-*@index: interger
-*@sum: some of intergers
 *Return: Returns sum of parameters
 */
 int sum_them_all(const unsigned int n, ...)
