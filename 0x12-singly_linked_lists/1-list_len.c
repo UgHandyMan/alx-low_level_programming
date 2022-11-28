9d9269d5ea6b3511a8ad32b5fd72493d64ca3660
UgHandyMan <fredkawesi@gmail.com.com>
2022-11-28 08:10:38 -0800

    singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *list_len - gets the number of nodes
+ *@h: pointer th the head
+ *Return:vreturns zero by default
+ */
+
+size_t list_len(const list_t *h)
+{
+  size_t node_count
