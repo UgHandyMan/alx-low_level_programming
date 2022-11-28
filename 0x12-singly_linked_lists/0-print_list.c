325c40b5e112439f5f60c1081f9bcdff395bf5c0
Kawesi Fred <113637771+UgHandyMan@users.noreply.github.com>
2022-11-28 19:01:27 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,18 +11,17 @@
 
 size_t print_list(const list_t *h)
 {
-size_t node_count = 1;
-if (h == null)
-return (0);
-
-while (h->next != NULL)
+const list_t *cursor = h;
+size_t count = 0;
+while (cursor != NULL)
 {
-if (h->str == NULL)
-printf("[%d] %s\n", 0);
-else printf("[%d] %s\n", h->len, h->str);
+if (cursor->str != NULL)
+printf("[%d] %s\n", cursor->len, cursor->str);
+else
+printf("[0] (nil)\n");
+count += 1;
+cursor = cursor->next
 }
-
-printf("[%d] %s\n", h->len, h->str);
-return (node_count);
+return (count)
 }
   
